53ccaa1be9624783b1b0b7f4f6cc8589150876d9
Nimposter <naelhumball@gmail.com>
2026-04-03 18:23:53 +0500

    Add files via upload

---
+++ Python notes.py
@@ -0,0 +1,24 @@
+print("string")
+#outputs the word string
+
+print(VariableName)
+#outputs whatever is stored in the variable VariableName
+
+VariableName = "hello"
+#Declares VariableName as string data type, stores the word hello in VariableName
+
+VariableName = 7
+#Declares VariableName as integer data type, stores the integer 7 in VariableName
+
+VariableName = variable2
+#stores the value of variable2 in VariableName
+
+VariableName = input("what would u like to store")
+#outputs what would u like to store, waits for user to enter something, and stores it in VariableName (treats whatever is entered as string)
+
+VariableName = int(input("what would u like to store"))
+#outputs what would u like to store, waits for user to enter something, and stores it in VariableName (treats whatever is entered as integer)
+#can also be float() etc 
+
+List = ["hello" , "hello"]
+#declares a list (one dimensional array) of string with index 0 as first index
